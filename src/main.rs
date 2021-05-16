af715c18cc9e498e44246cbfaefa63b53bfec003
Yutaka Kamei <kamei@yykamei.me>
2021-05-16 15:07:30 +0900

    Print only an error message at failure (without prefix)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     match entrypoint(&mut env::args_os()) {
         Ok(_) => safe_exit(0),
         Err(e) => {
-            eprintln!("Something happened: {:?}", e); // TODO: Write a more readable error message.
+            eprintln!("{}", e); // TODO: Write a more readable error message.
             safe_exit(e.exit_code);
         }
     }
