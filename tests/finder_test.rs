1d4ae8109caf2153b2559d84c78ac278c7ccaf37
Yutaka Kamei <kamei@yykamei.me>
2021-05-19 17:24:46 +0900

    Make tests DRY

--- tests/finder_test.rs
+++ tests/finder_test.rs
@@ -1,43 +1,12 @@
-use pinpoint::{Finder, MatchedPath, Result};
+use pinpoint::Finder;
 
 use crate::helper::create_tree;
 
 mod helper;
 
-#[test]
-fn returns_all_paths() {
-    let dir = create_tree().unwrap();
-    let finder = Finder::new(dir.path().to_str().unwrap(), "").unwrap();
-    let size = finder.collect::<Vec<Result<MatchedPath>>>().len();
-    assert_eq!(size, 29);
-}
-
-#[test]
-fn returns_empty() {
-    let dir = create_tree().unwrap();
-    let finder = Finder::new(
-        dir.path().to_str().unwrap(),
-        "the word should be not found with 🎂",
-    )
-    .unwrap();
-    let size = finder.collect::<Vec<Result<MatchedPath>>>().len();
-    assert_eq!(size, 0);
-}
-
-#[test]
-fn returns_filtered_paths_with_only_separator() {
-    let dir = create_tree().unwrap();
-    let finder = Finder::new(dir.path().to_str().unwrap(), "/").unwrap();
-    let size = finder.collect::<Vec<Result<MatchedPath>>>().len();
-    assert_eq!(size, 15);
-}
-
-#[test]
-fn returns_filtered_paths_with_uppercase() {
-    let dir = create_tree().unwrap();
-    let finder = Finder::new(dir.path().to_str().unwrap(), "licenSE").unwrap();
+fn find_paths(starting_point: &str, query: &str) -> Vec<String> {
     let mut paths = vec![];
-    for matched in finder {
+    for matched in Finder::new(starting_point, query).unwrap() {
         let path = matched.unwrap();
         paths.push(path);
     }
@@ -46,6 +15,38 @@ fn returns_filtered_paths_with_uppercase() {
         .map(|m| m.relative.replace('\\', "/"))
         .collect();
     paths.sort();
+    paths
+}
+
+#[test]
+fn returns_all_paths() {
+    let dir = create_tree().unwrap();
+    let size = find_paths(dir.path().to_str().unwrap(), "").len();
+    assert_eq!(size, 29);
+}
+
+#[test]
+fn returns_empty() {
+    let dir = create_tree().unwrap();
+    let size = find_paths(
+        dir.path().to_str().unwrap(),
+        "the word should be not found with 🎂",
+    )
+    .len();
+    assert_eq!(size, 0);
+}
+
+#[test]
+fn returns_filtered_paths_with_only_separator() {
+    let dir = create_tree().unwrap();
+    let size = find_paths(dir.path().to_str().unwrap(), "/").len();
+    assert_eq!(size, 15);
+}
+
+#[test]
+fn returns_filtered_paths_with_uppercase() {
+    let dir = create_tree().unwrap();
+    let paths = find_paths(dir.path().to_str().unwrap(), "licenSE");
     assert_eq!(paths.len(), 1);
     assert_eq!(paths, vec!["LICENSE"]);
 }
@@ -53,17 +54,7 @@ fn returns_filtered_paths_with_uppercase() {
 #[test]
 fn returns_filtered_paths_with_emoji_coffee() {
     let dir = create_tree().unwrap();
-    let finder = Finder::new(dir.path().to_str().unwrap(), "☕").unwrap();
-    let mut paths = vec![];
-    for matched in finder {
-        let path = matched.unwrap();
-        paths.push(path);
-    }
-    let mut paths: Vec<String> = paths
-        .iter()
-        .map(|m| m.relative.replace('\\', "/"))
-        .collect();
-    paths.sort();
+    let paths = find_paths(dir.path().to_str().unwrap(), "☕");
     assert_eq!(paths.len(), 3);
     assert_eq!(paths, vec!["lib/a/b/c/☕.js", "src/a/☕.js", "☕.txt"]);
 }
