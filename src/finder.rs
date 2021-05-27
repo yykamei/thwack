051be4dc0df958c6e834deb7860fa90f6434c6ca
Yutaka Kamei <kamei@yykamei.me>
2021-05-27 21:46:20 +0900

    Implement Ord for MatchedPath (#33)
    We can sort slices with this struct with the implementation of `Ord` for
    `MatchedPath`. This will be useful to show plausible file path
    candidates.

--- src/finder.rs
+++ src/finder.rs
@@ -1,8 +1,8 @@
 use std::collections::VecDeque;
-use std::fmt::{self, Debug, Display, Formatter};
 use std::path::{Path, PathBuf};
 
 use crate::error::{Error, Result};
+use crate::matched_path::MatchedPath;
 
 pub struct Finder {
     starting_point: String,
@@ -76,7 +76,7 @@ impl ConsumedDir {
                     return Err(Error::invalid_unicode(&format!(
                         "The path {:?} does not seem to be valid unicode.",
                         path
-                    )))
+                    )));
                 }
             };
             // TODO: Symbolic link?
@@ -102,48 +102,6 @@ impl Iterator for ConsumedDir {
     }
 }
 
-#[derive(Debug)]
-pub struct MatchedPath {
-    pub absolute: String,
-    pub relative: String,
-    pub positions: Vec<usize>,
-}
-
-impl Display for MatchedPath {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        Display::fmt(&self.relative, f)
-    }
-}
-
-impl MatchedPath {
-    pub(super) fn new(query: &str, starting_point: &str, absolute: &str) -> Option<Self> {
-        let relative: Vec<char> = absolute
-            .strip_prefix(starting_point)
-            .expect("The passed starting_point must be prefix of the path.")
-            .chars()
-            .collect();
-        let relative = &relative[1..]; // NOTE: Delete the prefix of slash
-        let mut positions: Vec<usize> = vec![];
-        for char in normalize_query(query).chars() {
-            let begin = if let Some(pos) = positions.last() {
-                pos + 1
-            } else {
-                0
-            };
-            // TODO: Explain this line later.
-            let target = &relative[begin..];
-            let pos = target.iter().position(|t| char.eq_ignore_ascii_case(t))?;
-            positions.push(begin + pos);
-        }
-        Some(Self {
-            absolute: absolute.to_string(),
-            relative: relative.iter().collect(),
-            positions,
-        })
-    }
-    // TODO: Present with colorized value with emphasized positions.
-}
-
 fn canonicalize_starting_point(path: &Path) -> Result<PathBuf> {
     path.canonicalize().map_err(|_e|
         Error::args(&format!(
@@ -151,16 +109,4 @@ fn canonicalize_starting_point(path: &Path) -> Result<PathBuf> {
             path,
         ))
     )
-}
-
-#[cfg(target_os = "windows")]
-fn normalize_query(query: &str) -> String {
-    // NOTE: Forward slashes are not allowed in a filename, so this replacing is supposed to work.
-    //       See https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
-    query.replace('/', "\\")
-}
-
-#[cfg(not(target_os = "windows"))]
-fn normalize_query(query: &str) -> &str {
-    query
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub use cli::entrypoint;
 pub use cli::safe_exit;
 pub use error::{Error, ErrorKind, Result};
-pub use finder::{Finder, MatchedPath};
+pub use finder::Finder;
+pub use matched_path::MatchedPath;
 
 mod args;
 mod cli;
 mod error;
 mod finder;
+mod matched_path;
