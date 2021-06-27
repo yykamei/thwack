940a0e5c2c2918c04c45103cedb9e5b2b20e7339
Yutaka Kamei <kamei@yykamei.me>
2021-06-27 20:58:19 +0900

    Use Array

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -3,13 +3,13 @@
 set -xeuo pipefail
 
 if [[ -z "${FUTURE_RELEASE:+y}" ]]; then
-  suffix=''
+  suffix=()
 else
-  suffix="--future-release ${FUTURE_RELEASE}"
+  suffix=(--future-release "${FUTURE_RELEASE}")
 fi
 
 github_changelog_generator \
   --user "${GITHUB_REPOSITORY%/*}" \
   --project "${GITHUB_REPOSITORY#*/}" \
   --exclude-labels chore,no-changelog,duplicate,question,invalid,wontfix \
-  "${suffix}"
+  "${suffix[@]}"
