4111ca1346d76a2b57a6105b972e2b67a74fca0c
Yutaka Kamei <kamei@yykamei.me>
2021-06-27 20:40:02 +0900

    Refactor GitHub Actions workflows
    * Add shell scripts to let use reuse them in multiple jobs
    * Change the behavior of changelog.yml to push changes directly

---
+++ scripts/git_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+changes=$(git status --short | wc -l)
+if [[ "$changes" == "0" ]]; then
+  exit 0
+fi
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add -A
+git commit -m "${COMMIT_MESSAGE}"
+git push
