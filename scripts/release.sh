cfc048c0896da68f234ba149541a6672498fb08d
Yutaka Kamei <kamei@yykamei.me>
2021-06-28 06:00:29 +0900

    Refactor GitHub Actions workflows (#104)
    * Add shell scripts to let us reuse them in multiple jobs
    * Change the behavior of changelog.yml to push changes directly
    * Remove `title` from inputs of workflow_dispatch

---
+++ scripts/release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+# Check whether the version is described in the source code.
+grep "$VERSION" Cargo.toml
+grep -F "## [${FUTURE_RELEASE}]" CHANGELOG.md
+grep -F '## [Unreleased]' CHANGELOG.md && exit 1
+
+# Extract the latest changelog entries to put them into GitHub release notes.
+hit=0
+while IFS="" read -r line; do
+  if [[ "$line" == "## ["*"]("*")"* ]]; then
+    hit=$((hit + 1))
+    continue
+  fi
+  if [[ "$hit" == "1" ]]; then
+    echo "$line" >> tmp/notes.txt
+  fi
+done < CHANGELOG.md
+
+cargo publish --dry-run
+cargo package --list
+gh release list
+cat tmp/notes.txt
+
+if [[ "$APPLY" == "true" ]]; then
+  gh release create "$FUTURE_RELEASE" --title "$RELEASE_TITLE" --notes-file tmp/notes.txt
+  cargo publish
+fi
