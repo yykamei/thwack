61c17914a987dda92089a0d6088f898f04335d9d
Yutaka Kamei <kamei@yykamei.me>
2021-06-28 06:33:13 +0900

    Fix changelog.yml to open PR instead of a direct push
    A direct push does not work within protected branches.
    I reverted the functionality with new script `gh_pr_create.sh`.

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -7,6 +7,9 @@ if [[ "$changes" == "0" ]]; then
   exit 0
 fi
 
+if [[ -n "${BRANCH:+y}" ]]; then
+  git checkout -b "$BRANCH"
+fi
 git config user.name github-actions
 git config user.email github-actions@github.com
 git add -A
