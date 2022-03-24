c7e32c298ce6086672d3c9d6ebc6ef93757abd08
Yutaka Kamei <kamei@yykamei.me>
2022-03-25 00:46:55 +0900

    Change Ord for MatchedPath (#243)
    I found out the `depth` of `MatchedPath` is not important, so I decided
    to just sort items by `relative` alphabetically.

--- tests/copy_selection.rs
+++ tests/copy_selection.rs
@@ -35,6 +35,8 @@ fn copy_with_absolute_path() {
             b"\x1b[1;1H\x1b[J",
             b"Search: \x1b7\x1b[1E",
             b"> .browserslistrc\x1b[1E",
+            b"  .config/bar.toml\x1b[1E",
+            b"  .config/ok.toml\x1b[1E",
             b"  .editorconfig\x1b[1E",
             b"  .env\x1b[1E",
             b"  .env.local\x1b[1E",
@@ -44,12 +46,10 @@ fn copy_with_absolute_path() {
             b"  Dockerfile\x1b[1E",
             b"  LICENSE\x1b[1E",
             b"  README.md\x1b[1E",
-            b"  package-lock.json\x1b[1E",
-            b"  package.json\x1b[1E",
-            b"  tsconfig.json\x1b[1E",
-            b"  \xe2\x98\x95.txt\x1b[1E",
-            b"  .config/bar.toml\x1b[1E",
-            b"  .config/ok.toml\x1b[1E",
+            b"  lib/a/b/c/index.js\x1b[1E",
+            b"  lib/a/b/c/\xe2\x98\x95.js\x1b[1E",
+            b"  lib/a/b/index.js\x1b[1E",
+            b"  lib/a/index.js\x1b[1E",
             b"  lib/bar.js\x1b[1E",
             b"\x1b[19d\x1b[1m\x1b[7m.browserslistrc                                                                                   \x1b[0m\x1b[1E",
             b"\x1b[1m<Up>/<Ctrl-p>:\x1b[0m\x1b[1CUp\x1b[2C\x1b[1m<Down>/<Ctrl-n>:\x1b[0m\x1b[1CDown\x1b[2C\x1b[1m<Enter>:\x1b[0m\x1b[1CExecute\x1b[2C\x1b[1m<C-d>/<C-y>:\x1b[0m\x1b[1CCopy (relative/absolute)",
@@ -57,7 +57,9 @@ fn copy_with_absolute_path() {
             b"\x1b[1;1H\x1b[J",
             b"Search: \x1b7\x1b[1E",
             b"  .browserslistrc\x1b[1E",
-            b"> .editorconfig\x1b[1E",
+            b"> .config/bar.toml\x1b[1E",
+            b"  .config/ok.toml\x1b[1E",
+            b"  .editorconfig\x1b[1E",
             b"  .env\x1b[1E",
             b"  .env.local\x1b[1E",
             b"  .gitignore\x1b[1E",
@@ -66,19 +68,21 @@ fn copy_with_absolute_path() {
             b"  Dockerfile\x1b[1E",
             b"  LICENSE\x1b[1E",
             b"  README.md\x1b[1E",
-            b"  package-lock.json\x1b[1E",
-            b"  package.json\x1b[1E",
-            b"  tsconfig.json\x1b[1E",
-            b"  \xe2\x98\x95.txt\x1b[1E",
-            b"  .config/bar.toml\x1b[1E",
-            b"  .config/ok.toml\x1b[1E",
+            b"  lib/a/b/c/index.js\x1b[1E",
+            b"  lib/a/b/c/\xe2\x98\x95.js\x1b[1E",
+            b"  lib/a/b/index.js\x1b[1E",
+            b"  lib/a/index.js\x1b[1E",
             b"  lib/bar.js\x1b[1E",
-            b"\x1b[19d\x1b[1m\x1b[7m.editorconfig                                                                                     \x1b[0m\x1b[1E",
+            b"\x1b[19d\x1b[1m\x1b[7m.config/bar.toml                                                                                  \x1b[0m\x1b[1E",
             b"\x1b[1m<Up>/<Ctrl-p>:\x1b[0m\x1b[1CUp\x1b[2C\x1b[1m<Down>/<Ctrl-n>:\x1b[0m\x1b[1CDown\x1b[2C\x1b[1m<Enter>:\x1b[0m\x1b[1CExecute\x1b[2C\x1b[1m<C-d>/<C-y>:\x1b[0m\x1b[1CCopy (relative/absolute)",
             b"\x1b8",
             b"\x1b[?1049l"
         )
     );
     let mut ctx = ClipboardContext::new().unwrap();
-    assert_eq!(ctx.get_contents().unwrap(), ".editorconfig");
+    if cfg!(windows) {
+        assert_eq!(ctx.get_contents().unwrap(), ".config\\bar.toml");
+    } else {
+        assert_eq!(ctx.get_contents().unwrap(), ".config/bar.toml");
+    }
 }
